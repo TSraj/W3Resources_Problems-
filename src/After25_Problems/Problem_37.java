89a0bc29e0400ccc89d528f6142bec9a99e70300
Md. Tanvir Sadik Raj <32238769+TSraj@users.noreply.github.com>
2022-06-12 00:55:24 +0600

    reversestringpb37

---
+++ src/After25_Problems/Problem_37.java
@@ -0,0 +1,18 @@
+package After25_Problems;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Problem_37 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the string: ");
+		char[] letters = input.nextLine().toCharArray();
+		
+		System.out.println("The reverse string: ");
+		for (int i= letters.length -1; i>=0; i--) {
+			System.out.println(letters[i]);
+		}
+//		System.out.println("\n");
+	}
+}
