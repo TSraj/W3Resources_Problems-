f26d3c445400aad06e15dcecef129ef639407e9c
Md. Tanvir Sadik Raj <32238769+TSraj@users.noreply.github.com>
2022-06-12 01:06:53 +0600

    problem48

---
+++ src/After25_Problems/Problem_48.java
@@ -0,0 +1,13 @@
+package After25_Problems;
+
+import java.util.Iterator;
+
+public class Problem_48 {
+	public static void main(String[] args) {
+		for(int i = 1; i<100; i++) {
+			if(i%2 !=0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
